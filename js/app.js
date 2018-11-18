adae94376e1c4ac7e6351791917310528cac260f
papermashea <molloy.shea@gmail.com>
2018-11-18 16:31:17 -0500

    finalizing form styles

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@ $(function () {
             $("#switch-form").show();
         });
     });
+
+$(function () {
+        $(".add").on('click', function () {
+            $("#switch2").show();
+        });
+    });
+
+$(function () {
+        $(".add").on('click', function () {
+            $(".cancel").show();
+        });
+    });
+
+
+$(function () {
+        $(".cancel").on('click', function () {
+            $("#switch2").hide();
+        });
+    });
